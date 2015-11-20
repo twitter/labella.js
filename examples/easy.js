5ba6d76a3228a8ece6ea9dee196016f4d49e605e
kwongsuphasawat <kwongsuphasawat@twitter.com>
2015-11-19 23:16:11 -0800

    use vanilla d3 in easy example instead of d3kit

--- examples/easy.js
+++ examples/easy.js
@@ -1,3 +1,4 @@
+// Create dummy data
 var nodes = [
   new labella.Node(1,50),
   new labella.Node(2,50),
@@ -16,30 +17,37 @@ var nodes = [
   new labella.Node(854,50)
 ];
 
-var skeleton = new d3Kit.Skeleton('#timeline',
-  {
-    margin: {left: 20, right: 20, top: 20, bottom: 20},
-    initialWidth: 1000,
-    initialHeight: 112,
-    labelHeight: 12
-  }
-);
+var options =   {
+  margin: {left: 20, right: 20, top: 20, bottom: 20},
+  initialWidth: 1000,
+  initialHeight: 112,
+  labelHeight: 12
+};
 
-var layers = skeleton.getLayerOrganizer();
-var options = skeleton.options();
-layers.create(['line', 'path', 'label', 'dot']);
-
+var innerWidth =  options.initialWidth - options.margin.left - options.margin.right;
+var innerHeight = options.initialHeight - options.margin.top - options.margin.bottom;
 var colorScale = d3.scale.category10();
+
+var vis = d3.select('#timeline')
+  .append('svg')
+    .attr('width',  options.initialWidth)
+    .attr('height', options.initialHeight)
+  .append('g')
+    .attr('transform', 'translate('+options.margin.left+','+options.margin.top+')');
 
 var renderer = new labella.Renderer({
   rowGap: 60,
   labelHeight: options.labelHeight
 });
 
-layers.get('line').append('line')
-  .attr('x2', skeleton.getInnerWidth())
+vis.append('line')
+  .attr('x2', innerWidth)
   .style('stroke-width', 2)
   .style('stroke', '#222');
+
+var linkLayer = vis.append('g');
+var labelLayer = vis.append('g');
+var dotLayer = vis.append('g');
 
 function color(d,i){
   return colorScale(i);
@@ -54,10 +62,9 @@ function draw(nodes){
   // Draw dots on the timeline
   // ---------------------------------------------------
 
-  var dots = layers.get('dot').selectAll('circle.dot')
-    .data(nodes);
-
-  dots.enter().append('circle')
+  dotLayer.selectAll('circle.dot')
+    .data(nodes)
+  .enter().append('circle')
     .classed('dot', true)
     .attr('r', 3)
     .attr('cx', function(d){return d.getRoot().idealPos;})
@@ -67,10 +74,9 @@ function draw(nodes){
   // Draw label rectangles
   // ---------------------------------------------------
 
-  var rect = layers.get('label').selectAll('rect.flag')
-    .data(nodes);
-
-  rect.enter().append('rect')
+  labelLayer.selectAll('rect.flag')
+    .data(nodes)
+  .enter().append('rect')
     .classed('flag', true)
     .attr('x', function(d){ return d.currentPos - d.width/2; })
     .attr('y', function(d){ return d.y; })
@@ -83,16 +89,14 @@ function draw(nodes){
   // Draw path from point on the timeline to the label rectangle
   // ---------------------------------------------------
 
-  var path = layers.get('path').selectAll('path')
-    .data(nodes);
-
-  path.enter().append('path')
+  linkLayer.selectAll('path')
+    .data(nodes)
+  .enter().append('path')
     .attr('d', function(d){return renderer.verticalPath(d);})
     .style('stroke', color)
     .style('stroke-width',2)
     .style('opacity', 0.6)
     .style('fill', 'none');
-
 }
 
 //---------------------------------------------------
