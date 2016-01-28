cbeb5b3b32f4d399409b4fde2f94e81788a6ec01
kwongsuphasawat <kwongsuphasawat@twitter.com>
2016-01-27 19:01:07 -0800

    update unit test

--- src/core/node.spec.js
+++ src/core/node.spec.js
@@ -18,4 +18,36 @@ describe('Node', function(){
       expect(n1.distanceFrom(n2)).toEqual(-10);
     });
   });
+
+  describe('moveToIdealPosition()', function(){
+    it('should change the current position to ideal position', function(){
+      var n = new Node(10,10);
+      n.currentPos = 20;
+      expect(n.currentPos).not.toEqual(n.idealPos);
+      n.moveToIdealPosition();
+      expect(n.currentPos).toEqual(n.idealPos);
+    });
+  });
+
+
+  describe('#displacement()', function(){
+    it('should return distance from ideal position', function(){
+      var n1 = new Node(10, 10);
+      expect(n1.displacement()).toEqual(0);
+      n1.currentPos = 20;
+      expect(n1.displacement()).toEqual(-10);
+      n1.currentPos = 0;
+      expect(n1.displacement()).toEqual(10);
+    });
+  });
+
+  describe('#overlapWithNode(node, buffer)', function(){
+
+  });
+
+  describe('overlapWithPoint(pos)', function(){
+
+  });
+
+
 });
